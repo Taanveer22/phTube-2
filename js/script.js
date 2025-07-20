e85f3a3e9e853e25c776883775ce7af1ecc8d856
Taanveer22 <taanveer469@gmail.com>
2025-07-20 07:49:45 +0600

    alahamdulliah data loaded from api

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-console.log('connected js');
+console.log("connected js");
+
+// step 01
+const loadCategories = () => {
+  fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
+    .then((response) => response.json())
+    .then((data) => displayCategories(data.categories))
+    .catch((error) => console.log(error));
+};
+
+// step 02
+const displayCategories = (data) => {
+  console.log(data);
+};
+
+// final function invocation
+loadCategories();
