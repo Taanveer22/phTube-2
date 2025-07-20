4b2468c835bdd745e5cc29ed952a7713458b5eec
Taanveer22 <taanveer469@gmail.com>
2025-07-20 09:05:15 +0600

    alhamdulliah videos card showed in the ui

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 console.log("connected js");
 
-// step 01
+// step 01 : load categories btn from api
 const loadCategories = () => {
   fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
     .then((response) => response.json())
@@ -8,18 +8,26 @@ const loadCategories = () => {
     .catch((error) => console.log(error));
 };
 
-// step 02
+// step 03 : load videos card from api
+const loadVideos = () => {
+  fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+    .then((response) => response.json())
+    .then((data) => displayVideos(data.videos))
+    .catch((error) => console.log(error));
+};
+
+// step 02 : display categories btn to ui
 const displayCategories = (data) => {
   //   console.log(data);
-  //   step 03
+  //   sub-step 02
   const categoryBtnContainer = document.getElementById(
     "category-btn-container"
   );
-  console.log(categoryBtnContainer);
+  //   console.log(categoryBtnContainer);
 
-  //   step 04
+  //   sub-step 02
   data.forEach((item) => {
-    console.log(item);
+    // console.log(item);
     const button = document.createElement("button");
     button.classList = "btn btn-error";
     button.innerText = item.category;
@@ -27,5 +35,40 @@ const displayCategories = (data) => {
   });
 };
 
+// step 04 : display videos card to the ui
+const displayVideos = (data) => {
+  //   console.log(data);
+  const videoCardContainer = document.getElementById("video-card-container");
+  data.forEach((item) => {
+    console.log(item);
+    const div = document.createElement("div");
+    div.classList = "card";
+    div.innerHTML = `
+        <figure class = "h-52">
+            <img
+                src = "${item.thumbnail}"
+                class = "h-full w-full object-cover"
+            />
+        </figure>
+        <section class = "px-0 py-2 flex gap-5">
+            <img
+                src = "${item.authors[0].profile_picture}"
+                class = "h-12 w-12 rounded-full object-cover"
+            />
+            <div>
+                <h2 class = "font-bold">${item.title}</h2>
+                <div class = "flex gap-2 items-center">
+                    <p>${item.authors[0].profile_name}</p>
+                    <img class = "w-5 h-5" src="https://img.icons8.com/?size=100&id=2AuMnRFVB9b1&format=png&color=000000">
+                </div>
+                <p>${item.others.views}</p>
+            </div>
+        </section>
+    `;
+    videoCardContainer.append(div);
+  });
+};
+
 // final function invocation
 loadCategories();
+loadVideos();
